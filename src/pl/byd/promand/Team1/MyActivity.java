37e9563c382e51cecd5d3f03a5c19a835c3c5433
Neruhys Rimdžius <tikpkr@gmail.com>
2013-03-14 14:53:32 +0200

    There are already workplace and we can add a properties on buttons for now.

--- src/pl/byd/promand/Team1/MyActivity.java
+++ src/pl/byd/promand/Team1/MyActivity.java
@@ -11,7 +11,7 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.options);
+        setContentView(R.layout.main);
 
     }
 }
