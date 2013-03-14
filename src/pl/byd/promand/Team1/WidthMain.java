ac2701152084d2e51620c9766153449d2e59f764
Mark-Hendrik Mäeste <mark.maeste@gmail.com>
2013-03-14 20:08:47 +0200

    Tools and Width

---
+++ src/pl/byd/promand/Team1/WidthMain.java
@@ -0,0 +1,56 @@
+package pl.byd.promand.Team1;
+
+//import com.example.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class WidthMain extends Activity {
+
+	SeekBar bar;
+	TextView txtNumbers;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_width_main);
+		bar=(SeekBar) findViewById(R.id.seekbar);
+		txtNumbers=(TextView)findViewById(R.id.TextView01);
+		bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+		
+			@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+				// TODO Auto-generated method stub
+				
+				txtNumbers.setText(String.valueOf(arg1));
+				
+				/* mProgressText.setText(progress + " " + 
+                "from touch" + "=" + fromTouch); */
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.width_main, menu);
+		return true;
+	}
+	
+
+}
