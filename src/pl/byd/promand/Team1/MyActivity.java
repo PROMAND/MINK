ae95d353aac1a28a8c61534d310927ccbf520ba5
lani13 <kate_lani@wp.pl>
2013-03-14 20:41:08 +0100

    Update

--- src/pl/byd/promand/Team1/MyActivity.java
+++ src/pl/byd/promand/Team1/MyActivity.java
@@ -1,8 +1,10 @@
 package pl.byd.promand.Team1;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import com.promand.Team1.R;
@@ -11,18 +13,50 @@ public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+      Context context = this;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button button4 = (Button) findViewById(R.id.button4);
+        Button tools = (Button) findViewById(R.id.button1);
+        Button width = (Button) findViewById(R.id.button2);
+        Button colors = (Button) findViewById(R.id.button3);
+        Button settings = (Button) findViewById(R.id.button4);
 
-        button4.setOnClickListener(new View.OnClickListener() {
+        tools.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+                Intent i = new Intent(context, Tools.class);
                 startActivity(i);
-            }});
+            }
+        });
+
+        width.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(context, WidthMain.class);
+                startActivity(i);
+            }
+        });
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(context, ColorChange.class);
+                startActivity(i);
+            }
+        });
+
+
+        settings.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(context, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
+
 }
