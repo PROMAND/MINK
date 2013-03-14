bcdd75c26e00b50ff10d41eda4116a70a4a58c02
lani13 <kate_lani@wp.pl>
2013-03-14 13:17:30 +0100

    Update

--- src/pl/byd/promand/Team1/MyActivity.java
+++ src/pl/byd/promand/Team1/MyActivity.java
@@ -11,7 +11,7 @@ public class MyActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.options);
 
     }
 }
