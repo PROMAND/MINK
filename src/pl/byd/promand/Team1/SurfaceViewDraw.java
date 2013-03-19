bd8fbdfe2f8b1b6513ef8230623a4d28eb10ae9a
lani13 <kate_lani@wp.pl>
2013-03-19 11:43:10 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/pl/byd/promand/Team1/SurfaceViewDraw.java

--- src/pl/byd/promand/Team1/SurfaceViewDraw.java
+++ src/pl/byd/promand/Team1/SurfaceViewDraw.java
@@ -1,102 +1,120 @@
-/*package pl.byd.promand.Team1;
+package pl.byd.promand.Team1;
 
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
+import android.graphics.*;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import com.promand.Team1.R;
 
-import java.util.ArrayList;
 
-public class SurfaceViewDraw extends SurfaceView implements Runnable {
+public class SurfaceViewDraw extends SurfaceView {
 
 
     SurfaceHolder surfHolder;
-    Thread drawingThread;
-    boolean isRunning = false;
-    Bitmap photoBitmap;
+ //   Bitmap photoBitmap;
+   int counter = 0;
 
-      // constructor no. 1 - empty surface
-    public SurfaceViewDraw(Context context){
+    Bitmap mergedBitmap =null; //this bitmap here contains the whole of the drawing (background+foreground) to be saved.
+
+   Bitmap bitmap = null; //bitmap onto which we draw our stuff
+   // Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blank);
+
+  //  Canvas canvas - Main canvas. Will be linked to a .bmp file
+
+    int mBackgroundColor = Color.WHITE; //default background color
+    Paint mDefaultPaint = new Paint();
+
+
+    Paint mDrawPaint = new Paint(); //used for painting example foreground stuff... We draw line segments.
+    Point drawPoints = new Point(); //used to store last location on our PaintView that was finger-touched
+
+      // constructor - empty surface
+     public SurfaceViewDraw(Context context, int width, int height){
         super(context);
         surfHolder = getHolder();
+     }
 
-    }
-
-    // constructor no. 2 - surface with photo
-    public SurfaceViewDraw(Context context, Bitmap bitmap){
-        super(context);
-        surfHolder = getHolder();
-        photoBitmap = Bitmap.createBitmap(bitmap) ;
-
-    }
-
-
-    public void run()
+    /*
+    public void drawOnSurface(float x, float y, float r, Paint mPaint)
     {
-        while(isRunning)
-        {
-            if(!surfHolder.getSurface().isValid())
-                continue;
 
-            Canvas canvas = surfHolder.lockCanvas();
-            if (photoBitmap==null)
+          //  if(!surfHolder.getSurface().isValid())
+
+       Canvas canvas = surfHolder.lockCanvas();
+       if (photoBitmap==null)
+        {
             {
                 if(x!=0 && y!=0)
-                {
-                    canvas.drawRGB(255,250,240);
-                    canvas.drawCircle(x,y,60,new Paint());
-                }
+                    canvas.drawCircle(x,y,r*5,mPaint);
+
             }
 
-            else canvas.setBitmap(photoBitmap);
-
-
-            surfHolder.unlockCanvasAndPost(canvas);
         }
+
+
+       surfHolder.unlockCanvasAndPost(canvas);
+     }       */
+
+
+
+    //Called by user of PaintView in order to start a painting "stroke" (finger touching touch-screen): stores the
+    //location of the finger when it first touched the screen
+    public void startDraw(int x, int y, int radius, int color, int backColor){
+        mDrawPaint.setColor(color);
+       // mDrawPaint.setStyle(Paint.Style.FILL);
+       // mDrawPaint.setStrokeWidth(radius);
+        drawPoints.x = x;
+        drawPoints.y = y;
+        if (counter==0)
+          {changeColor(backColor);
+             ++counter;}
     }
 
-    public void pause()
-    {
-        isRunning = false;
-        while(true)
-        {
-            try {
-                drawingThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            break;
-        }
-        drawingThread = null;
+    //Called by user of PaintView when finger touching touch-screen is moving (must be called after a startDraw,
+    //as the latter initializes a couple of necessary things)
+    public void continueDraw(int x, int y, int radius){
+
+        Canvas canvas = surfHolder.lockCanvas();
+        //canvas.drawLine(drawPoints.x, drawPoints.y, x, y, mDrawPaint);
+        canvas.drawCircle(x,y,radius*5,mDrawPaint);
+        surfHolder.unlockCanvasAndPost(canvas);
+        drawPoints.x = x;
+        drawPoints.y = y;
+     //   postInvalidate(); //refresh view: this will indirectly invoke onDraw soon afterwards
     }
 
-    public void resume()
-    {
-        isRunning = true;
-        drawingThread = new Thread(this);
-        drawingThread.start();
-
+    //Merge the foreground Canvas-Bitmap with a solid background color, then stores this in the 2nd Canvas-Bitmap pair.
+    private void mergeLayers(){
+        Canvas mergedCanvas = surfHolder.lockCanvas();
+        mergedCanvas.drawRGB(255,250,240);
+        //mergedCanvas.drawColor(mBackgroundColor);
+        mergedCanvas.drawBitmap(bitmap, 0, 0, mDefaultPaint);
+        surfHolder.unlockCanvasAndPost(mergedCanvas);
     }
 
+    //Change background color
+    public void changeColor(int newColor){
+      //  mBackgroundColor = Color.WHITE;
+        Canvas mergedCanvas = surfHolder.lockCanvas();
+        mergedCanvas.drawRGB(255,250,240);
+        surfHolder.unlockCanvasAndPost(mergedCanvas);
+    //    postInvalidate(); //refresh view: this will indirectly invoke onDraw soon afterwards
+   }
 
-    @Override
+    /*
+       //   Bitmap myBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blank);
+     //   canvas.drawBitmap(myBitmap, 0, 0, null);
+             */
+
+  /*  @Override
     public void draw(Canvas canvas) {
-
         super.draw(canvas);
-        synchronized(pointsToDraw)
-        {
-            for (Path path : pointsToDraw) {
-                canvas.drawPath(path, mPaint);
-            }
-        }
-    }
-
-
-
-
-
+     //   mergeLayers();
+        canvas = surfHolder.lockCanvas();
+        canvas.drawBitmap(bitmap, 0, 0, mDefaultPaint);
+        surfHolder.unlockCanvasAndPost(canvas);
+    }              */
 }
-         */
+
